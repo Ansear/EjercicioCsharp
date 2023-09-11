ced0c653e91ad552d4d836f9f79542d78f9be2c4
sebastianarciniegas2003 <sebastianarciniegas2003@gmail.com>
2023-09-10 23:34:36 -0500

    feat: :sparkles: Mostrar los estudiantes con notas definitivas
    Se agrego la funcionalidad para mostrar los estudiantes con sus notas definitivas, con su nota final

--- Program.cs
+++ Program.cs
@@ -34,27 +34,7 @@ internal class Program
                         ShowStudents(estudiantes,notas);
                         break;
                     case 4:
-                        Console.Clear();
-                        foreach (NotasEstu item in notas)
-                        {
-                            Console.WriteLine(item.IdEstudiante);
-                            
-                                foreach (int vak in item.Quices)
-                                {        
-                                    Console.WriteLine(vak);
-                                }
-                                foreach (int vak in item.Parciales)
-                                {        
-                                    Console.WriteLine(vak);
-                                }
-                                foreach (int vak in item.Trabajos)
-                                {        
-                                    Console.WriteLine(vak);
-                                }
-
-                        }
-
-                        Console.ReadKey();
+                        ShowStudentsDef(estudiantes,notas);
                         break;
                     case 5:
                         Console.WriteLine("Gracias por usar el programa");
@@ -74,7 +54,6 @@ internal class Program
             }
         } while (val != 5);
     }
-
     public static void AddStudent(List<Estudiante> estudiantes,List<NotasEstu> notas){
         Console.Clear();
         bool ban;
@@ -160,11 +139,11 @@ internal class Program
         estudiantes.Add(estudiant);
         notas.Add(notasEst);
     }
-
     public static void AddNotas(List<NotasEstu> p1){
         Console.Clear();
         Console.WriteLine("Ingrese el id del estudiante: ");
         string idBusca = Console.ReadLine();
+        bool ban = false;
         foreach (NotasEstu item in p1)
         {
             if(item.IdEstudiante == idBusca){
@@ -199,85 +178,176 @@ internal class Program
                         Console.WriteLine("Valor ingresado invalido");
                         break;
                 }   
+                ban=true;
+                break;
+            }
+            else{
+                ban=false;
             }
         }
-        
-        
-    }
-
-    public static void ShowStudents(List<Estudiante> estudiantes, List<NotasEstu> notas){
-        Console.Clear();
-        Console.WriteLine("{0,-28} {1,13} {2,32} {3,12} {4,12}","Codigo","Nombre","Quices","Trabajos","Parciales");
-        Console.WriteLine("{0,68} {1,2} {2,2} {3,-5} {4,3} {5,-8} {6,2} {7,2} {8,2}","Q1","Q2","Q3","Q4","T1","T2","P1","P2","P3");
-        Console.WriteLine(" ");
-        foreach (Estudiante est in estudiantes)
-        {
-            float[] q = new float[4]{0,0,0,0};
-            float[] t = new float[2]{0,0};
-            float[] p = new float[3]{0,0,0};
-            foreach (NotasEstu item in notas)
-            {
-                if(item.IdEstudiante == est.Id){
-                    
-                        if(item.Quices.Count != 0){
-                            for(int i=0; i<item.Quices.Count; i++){
-                                    q[i]= item.Quices[i];
-                            }
-                        }
-                        if(item.Trabajos.Count != 0){
-                            for(int i=0; i<item.Trabajos.Count; i++){
-                                    t[i]= item.Trabajos[i];
-                            }
-                        }
-                        if(item.Parciales.Count != 0){
-                            for(int i=0; i<item.Parciales.Count; i++){
-                                    p[i]= item.Parciales[i];
-                            }
-                            
-                        }
-                }
-            }
-            Console.WriteLine("{0,-18} {1,40} {2,8}{3,3}{4,3}{5,3} {6,6}{7,3} {8,8}{9,3}{10,3}",
-            est.Id,
-            est.Nombre,
-            q[0]==0?"Np":q[0],
-            q[1]==0?"Np":q[1],
-            q[2]==0?"Np":q[2],
-            q[3]==0?"Np":q[3],
-            t[0]==0?"Np":t[0],
-            t[1]==0?"Np":t[1],
-            p[0]==0?"Np":p[0],
-            p[1]==0?"Np":p[1],
-            p[2]==0?"Np":p[2]);
+        if(ban != true){
+            Console.Clear();
+            Console.WriteLine("No se encontro el id ingresado");
+            Console.ReadKey();
         }
-        Console.ReadKey();
+        
     }
-
     public static void AddQuices(NotasEstu notasQ){
         if(notasQ.Quices.Count<4){
                 int toNo = notasQ.Quices.Count;
-                Console.WriteLine($"Agregar notas del Quiz {toNo+1}");
-                notasQ.Quices.Add(Convert.ToInt16(Console.ReadLine()));
+                Console.WriteLine($"Agregar notas del Quiz {toNo+1}, de 0 a 5,0");
+                float v = float.Parse(Console.ReadLine());
+                if(v>0 && v<=5){
+                    notasQ.Quices.Add(v);
+                }else{
+                    Console.WriteLine("Nota invalida");
+                }
         }else{
             Console.WriteLine("Las notas de Quices ya estan completas");
         }
     }
     public static void AddTrabajos(NotasEstu notasQ){
-        if(notasQ.Trabajos.Count()<2){
-                int toNo = notasQ.Trabajos.Count();
-                Console.WriteLine($"Agregar notas del Trabajo {toNo+1}");
-                notasQ.Trabajos.Add(Convert.ToInt16(Console.ReadLine()));
+        if(notasQ.Trabajos.Count<2){
+                int toNo = notasQ.Trabajos.Count;
+                Console.WriteLine($"Agregar notas del Trabajo {toNo+1}, de 0 a 5,0");
+                float v = float.Parse(Console.ReadLine());
+                if(v>0 && v<=5){
+                    notasQ.Trabajos.Add(v);
+                }else{
+                    Console.WriteLine("Nota invalida");
+                }
         }else{
             Console.WriteLine("Las notas de Trabajos ya estan completas");
         }
     }
     public static void AddParciales(NotasEstu notasQ){
-        if(notasQ.Parciales.Count()<3){
-                int toNo = notasQ.Parciales.Count();
-                Console.WriteLine($"Agregar notas del Parciales {toNo+1}");
-                notasQ.Parciales.Add(Convert.ToInt16(Console.ReadLine()));
+        if(notasQ.Parciales.Count<3){
+                int toNo = notasQ.Parciales.Count;
+                Console.WriteLine($"Agregar notas del Parciales {toNo+1}, de 0 a 5,0");
+                float v = float.Parse(Console.ReadLine());
+                if(v>0 && v<=5){
+                    notasQ.Parciales.Add(v);
+                }else{
+                    Console.WriteLine("Nota invalida");
+                }
         }else{
             Console.WriteLine("Las notas de Parciales ya estan completas");
         }
     }
+    public static void ShowStudents(List<Estudiante> estudiantes, List<NotasEstu> notas){
+        Console.Clear();
+        Console.WriteLine("{0,-28} {1,13}{2,33} {3,18} {4,18}","Codigo","Nombre","Quices","Trabajos","Parciales");
+        Console.WriteLine("{0,65}{1,5}{2,5}{3,5} {4,8}{5,5} {6,10}{7,5}{8,5}","Q1","Q2","Q3","Q4","T1","T2","P1","P2","P3");
+        Console.WriteLine(" ");
+        if(estudiantes.Count != 0){
+            foreach (Estudiante est in estudiantes)
+            {
+                float[] q = new float[4]{0,0,0,0};
+                float[] t = new float[2]{0,0};
+                float[] p = new float[3]{0,0,0};
+                foreach (NotasEstu item in notas)
+                {
+                    if(item.IdEstudiante == est.Id){
+                        
+                            if(item.Quices.Count != 0){
+                                for(int i=0; i<item.Quices.Count; i++){
+                                        q[i]= item.Quices[i];
+                                }
+                            }
+                            if(item.Trabajos.Count != 0){
+                                for(int i=0; i<item.Trabajos.Count; i++){
+                                        t[i]= item.Trabajos[i];
+                                }
+                            }
+                            if(item.Parciales.Count != 0){
+                                for(int i=0; i<item.Parciales.Count; i++){
+                                        p[i]= item.Parciales[i];
+                                }
+                                
+                            }
+                    }
+                }
+                Console.WriteLine("{0,-18}{1,40}{2,8}|{3,4}|{4,4}|{5,4}| {6,6}|{7,4}| {8,8}|{9,4}|{10,4}|",
+                est.Id,
+                est.Nombre,
+                q[0]==0?"Np":q[0],
+                q[1]==0?"Np":q[1],
+                q[2]==0?"Np":q[2],
+                q[3]==0?"Np":q[3],
+                t[0]==0?"Np":t[0],
+                t[1]==0?"Np":t[1],
+                p[0]==0?"Np":p[0],
+                p[1]==0?"Np":p[1],
+                p[2]==0?"Np":p[2]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Np = No Presento");
+        }else{
+            Console.WriteLine("En el momento no hay estudiantes registrados");
+        }
+        Console.ReadKey();
+    }
+    public static void ShowStudentsDef(List<Estudiante> estudiantes, List<NotasEstu> notas){
+        Console.Clear();
+        Console.WriteLine("{0,-28} {1,13} {2,33} {3,15} {4,15} {5,15}","Codigo","Nombre","Def Quices","Def Trabajos","Def Parciales","Nota Final");
+        Console.WriteLine(" ");
+        if(estudiantes.Count != 0){
+            foreach (Estudiante est in estudiantes)
+            {
+                float[] q = new float[4]{0,0,0,0};
+                float[] t = new float[2]{0,0};
+                float[] p = new float[3]{0,0,0};
+                float Tq = 0;
+                float Tt = 0;
+                float Tp = 0;
+                foreach (NotasEstu item in notas)
+                {
+                    if(item.IdEstudiante == est.Id){
+                        
+                            if(item.Quices.Count != 0){
+                                for(int i=0; i<item.Quices.Count; i++){
+                                        q[i]= item.Quices[i];
+                                }
+                            }
+                            if(item.Trabajos.Count != 0){
+                                for(int i=0; i<item.Trabajos.Count; i++){
+                                        t[i]= item.Trabajos[i];
+                                }
+                            }
+                            if(item.Parciales.Count != 0){
+                                for(int i=0; i<item.Parciales.Count; i++){
+                                        p[i]= item.Parciales[i];
+                                }
+                                
+                            }
+                    }
+                }
+                foreach (float item in q)
+                {
+                    Tq+=item;
+                }
+                foreach (float item in t)
+                {
+                    Tt+=item;
+                }
+                foreach (float item in p)
+                {
+                    Tp+=item;
+                }
+
+                float to = (Tp+Tq+Tt)/3;
+                Console.WriteLine("{0,-18} {1,40}| {2,12}| {3,12}| {4,12}| {5,15}|",
+                est.Id,
+                est.Nombre,
+                (Tq/4).ToString("F1"),
+                (Tt/2).ToString("F1"),
+                (Tp/3).ToString("F1"),
+                to.ToString("F1"));
+            }
+        }else{
+            Console.WriteLine("En el momento no hay estudiantes registrados");
+        }
+        Console.ReadKey();
+    }
+    
 }
